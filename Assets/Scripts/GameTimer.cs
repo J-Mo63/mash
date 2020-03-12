24d483d9e768ee4a515f641ce8fb9a8ea9570fcf
Jonathan Moallem <jdmoallem@gmail.com>
2020-03-12 13:25:01 +1100

    Added game timer to game

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    private float _time;
+    private Text _text;
+
+    private void Start()
+    {
+        _time = Time.timeSinceLevelLoad;
+        _text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        _time += Time.deltaTime;
+        _text.text = Mathf.RoundToInt(_time) + "s";
+    }
+}
